5fdd80b70854086433a6d9a88ebdd0cdc78e6fb2
Dmitry <55196418+Nzcheinenm@users.noreply.github.com>
2021-04-19 12:40:53 +0300

    BEM

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // const div = document.querySelector('div');
 // div.setAttribute('id','myid');
-// div.innerHTML = "<div class='div_inner'>Text</div>";
+// div.innerHTML = "<div class='div-inner'>Text</div>";
 
 // let inner = document.querySelector(".div_inner");
-// inner.insertAdjacentHTML("afterend","<span class='span_class'>Span1</span>");
-// inner.insertAdjacentHTML("afterend","<span class='span_class'>Span2</span>");
+// inner.insertAdjacentHTML("afterend","<span class='span-class'>Span1</span>");
+// inner.insertAdjacentHTML("afterend","<span class='span-class'>Span2</span>");
 // inner.insertAdjacentHTML("afterend","<div id='text1'>Div3</div>");
 // inner.insertAdjacentHTML("afterend","<div id='text1'>Div4</div>");
-
 
 //let ul = document.querySelector('ul');
 //ul.setAttribute('id','flex');
 //document.querySelectorAll('li').forEach(function (value,key) {value.setAttribute('id','li')});
 
-
-const div = document.querySelector('.header_button button');
-div.onclick = function() {
-    document.location='index.html#contact';
-}
+const div = document.querySelector(".header_button button");
+div.onclick = function () {
+  document.location = "index.html#contact";
+};
